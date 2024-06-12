cc5a388c3b92397f79fa9878c59b3b087fcca197
huayuenh <huayuenh@ie.ibm.com>
2024-06-12 21:09:39 +0100

    chore: signing test

--- gpg-key/scripts/gpg_keys.sh
+++ gpg-key/scripts/gpg_keys.sh
@@ -12,27 +12,25 @@ function parse_input() {
     #Generate a file to pass into GPG cli
     touch "${FILE}"
     {
-      echo "%echo Generating GPG key"
-      echo "Key-Type: RSA"
+      echo "Key-Type: 1"
       echo "Key-Length: 2048"
-      echo "Subkey-Type: RSA"
+      echo "Subkey-Type: 1"
       echo "Subkey-Length: 2048"
       echo "Name-Real: ${NAME}"
       echo "Name-Email: ${EMAIL}"
+      echo "Expire-Date: 0"
       echo "%no-ask-passphrase"
       echo "%no-protection"
-      echo "%commit"
-      echo "%echo done"
     } >> "${FILE}"
 
     #Generate a GPG key
     gpg --batch --gen-key "${FILE}"
     #Export the signing key
-    SIGNING_KEY=$(gpg --export-secret-key --export-options export-minimal "${EMAIL}" | base64)
-    SIGNING_KEY=$( echo -n "${SIGNING_KEY}" | tr '\n' '@' | sed -E 's/@//g' )
+    SIGNING_KEY=$(gpg --export-secret-key "${EMAIL}" | base64)
+    #SIGNING_KEY=$( echo -n "${SIGNING_KEY}" | tr '\n' '@' | sed -E 's/@//g' )
     #Export the public signing certifacate
     PUBLIC_CERTIFICATE=$(gpg --armor --export "${EMAIL}" | base64)
-    PUBLIC_CERTIFICATE=$( echo -n "${PUBLIC_CERTIFICATE}" | tr '\n' '@' | sed -E 's/@//g' )
+    #PUBLIC_CERTIFICATE=$( echo -n "${PUBLIC_CERTIFICATE}" | tr '\n' '@' | sed -E 's/@//g' )
 
     #Terraform requires a JSON response from a script
     JSON_STRING_RESULT=$( jq -n --arg signing_key "$SIGNING_KEY" --arg public_key "$PUBLIC_CERTIFICATE" '{signingkey: $signing_key, publickey: $public_key}' )

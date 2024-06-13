84d2cd0ecf4a937f318aef2e9a86b10da4fce39a
huayuenh <huayuenh@ie.ibm.com>
2024-06-13 14:07:39 +0100

    chore: test

--- gpg-key/outputs.tf
+++ gpg-key/outputs.tf
@@ -5,11 +5,9 @@
 output "gpg_key" {
   description = "The GPG signing key in base64 encoding."
   value       = data.external.generate_gpg_secrets.result.signingkey
-  sensitive   = true
 }
 
 output "gpg_public_certificate" {
   description = "The GPG public certificate in base64 encoding."
   value       = data.external.generate_gpg_secrets.result.publickey
-  sensitive   = true
 }

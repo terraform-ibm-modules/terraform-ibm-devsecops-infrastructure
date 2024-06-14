5e62c30e22aefab43057d46c4ff0c93ff0a87575
huayuenh <huayuenh@ie.ibm.com>
2024-06-14 18:12:16 +0100

    chore: partially working

--- gpg-key/scripts/gpg_keys.sh
+++ gpg-key/scripts/gpg_keys.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-dnf install pinentry -y
+#dnf install pinentry -y
+#dnf install expect -y
 
 function parse_input() {
   eval "$(jq -r '@sh "export EMAIL=\(.email) NAME=\(.name)"')"
@@ -8,46 +9,47 @@ function parse_input() {
   if [[ -z "${NAME}" ]]; then export NAME=none; fi
 }
 
- function generate_keys() {
-    FILE="gpg_file.txt"
+export EMAIL="huayuenh@ie.ibm.com"
+export NAME="hyhyh"
+function createKey {
+    /usr/bin/expect << EOF 
+        set timeout -1
+        spawn bash
+        send "gpg --pinentry-mode loopback --passphrase='' --generate-key\r"
+        expect "Real name: "
+        send -- "$NAME\n"
+        expect "Email address: "
+        send -- "$EMAIL\n"
+        expect "Change (N)ame, (E)mail, or (O)kay/(Q)uit? "
+        send -- "o\r"
+        expect eof
+        close
+EOF
+}
 
-    #Generate a file to pass into GPG cli
-    touch "${FILE}"
-    {
-      echo "Key-Type: 1"
-      echo "Key-Length: 2048"
-      echo "Subkey-Type: 1"
-      echo "Subkey-Length: 2048"
-      echo "Name-Real: ${NAME}"
-      echo "Name-Email: ${EMAIL}"
-      echo "Expire-Date: 0"
-      echo "%no-ask-passphrase"
-      echo "%no-protection"
-    } >> "${FILE}"
+function generate_keys() {
+  KEY_LIST=$(gpg --list-secret-keys)
 
+  if [[ "${KEY_LIST}" != *"${EMAIL}"* ]]; then
+    $(createKey) &
+  fi
 
+  #sleep 10
 
-    KEY_LIST=$(gpg --list-secret-keys)
+  #Export the signing key
+  SIGNING_KEY=$(gpg --export-secret-key "${EMAIL}" | base64)
+  #SIGNING_KEY=$( echo -n "${SIGNING_KEY}" | tr '\n' '@' | sed -E 's/@//g' )
+  #Export the public signing certifacate
+  PUBLIC_CERTIFICATE=$(gpg --armor --export "${EMAIL}" | base64)
+  #PUBLIC_CERTIFICATE=$( echo -n "${PUBLIC_CERTIFICATE}" | tr '\n' '@' | sed -E 's/@//g' )
 
-    #Generate the key if not found
-    if [[ "${KEY_LIST}" != *"${EMAIL}"* ]]; then
-        gpg --batch --gen-key "${FILE}"
-    fi
+  #Terraform requires a JSON response from a script
+  JSON_STRING_RESULT=$( jq -n --arg signing_key "$SIGNING_KEY" --arg public_key "$PUBLIC_CERTIFICATE" '{signingkey: $signing_key, publickey: $public_key}' )
+  #clean up
+  #rm -rf "${FILE}"
+  #return response
+  echo "${JSON_STRING_RESULT}"
+}
 
-    #Export the signing key
-    SIGNING_KEY=$(gpg --export-secret-key "${EMAIL}" | base64 -w0)
-    #SIGNING_KEY=$( echo -n "${SIGNING_KEY}" | tr '\n' '@' | sed -E 's/@//g' )
-    #Export the public signing certifacate
-    PUBLIC_CERTIFICATE=$(gpg --armor --export "${EMAIL}" | base64 -w0)
-    #PUBLIC_CERTIFICATE=$( echo -n "${PUBLIC_CERTIFICATE}" | tr '\n' '@' | sed -E 's/@//g' )
-
-    #Terraform requires a JSON response from a script
-    JSON_STRING_RESULT=$( jq -n --arg signing_key "$SIGNING_KEY" --arg public_key "$PUBLIC_CERTIFICATE" '{signingkey: $signing_key, publickey: $public_key}' )
-    #clean up
-    rm -rf "${FILE}"
-    #return response
-    echo "${JSON_STRING_RESULT}"
- }
-
-parse_input
+#parse_input
 generate_keys

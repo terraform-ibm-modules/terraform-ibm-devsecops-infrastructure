a88bcc085990c8e8f933df4021f0591060bcb8ed
huayuenh <huayuenh@ie.ibm.com>
2024-06-15 23:43:06 +0100

    chore: fix signing

--- gpg-key/scripts/gpg_keys.sh
+++ gpg-key/scripts/gpg_keys.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+dnf install pinentry -y
+
 function parse_input() {
   eval "$(jq -r '@sh "export EMAIL=\(.email) NAME=\(.name)"')"
   if [[ -z "${EMAIL}" ]]; then export EMAIL=none; fi
   if [[ -z "${NAME}" ]]; then export NAME=none; fi
 }
 
- function generate_keys() {
-    FILE="gpg_file.txt"
-
-    #Generate a file to pass into GPG cli
-    touch "${FILE}"
+function createKey {
+  FILE="gpg_file.txt"
+  touch "${FILE}"
     {
       echo "%echo Generating GPG key"
       echo "Key-Type: RSA"
@@ -28,20 +28,29 @@ function parse_input() {
       echo "%echo done"
     } >> "${FILE}"
 
-    #Generate a GPG key
     gpg --batch --gen-key "${FILE}"
-    #Export the signing key
-    SIGNING_KEY=$(gpg --export-secret-key "${EMAIL}" | base64 -w0)
-    #Export the public signing certifacate
-    PUBLIC_CERTIFICATE=$(gpg --armor --export "${EMAIL}" | base64 -w0)
-
-    #Terraform requires a JSON response from a script
-    JSON_STRING_RESULT=$( jq -n --arg signing_key "$SIGNING_KEY" --arg public_key "$PUBLIC_CERTIFICATE" '{signingkey: $signing_key, publickey: $public_key}' )
-    #clean up
     rm -rf "${FILE}"
-    #return response
-    echo "${JSON_STRING_RESULT}"
- }
+}
+
+function generate_keys() {
+  KEY_LIST=$(gpg --list-secret-keys)
+
+  if [[ "${KEY_LIST}" != *"${EMAIL}"* ]]; then
+    $(createKey)
+  fi
+
+  
+  #Export the signing key
+  SIGNING_KEY=$(gpg --export-secret-key "${EMAIL}" | base64 -w0)
+  #Export the public signing certifacate
+  PUBLIC_CERTIFICATE=$(gpg --armor --export "${EMAIL}" | base64 -w0)
+  #Terraform requires a JSON response from a script
+  JSON_STRING_RESULT=$( jq -n --arg signing_key "$SIGNING_KEY" --arg public_key "$PUBLIC_CERTIFICATE" '{signingkey: $signing_key, publickey: $public_key}' )
+
+  #return response
+ 
+  echo "${JSON_STRING_RESULT}"
+}
 
 parse_input
 generate_keys

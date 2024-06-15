d821e615188d998a8cb346c421c32921ab6a2a6b
huayuenh <huayuenh@ie.ibm.com>
2024-06-15 01:37:33 +0100

    chore: test

--- gpg-key/scripts/gpg_keys.sh
+++ gpg-key/scripts/gpg_keys.sh
@@ -12,24 +12,40 @@ function parse_input() {
 SIGNING_KEY="placeholder"
 PUBLIC_CERTIFICATE="placeholder"
 function createKey {
-    /usr/bin/expect << END 
-        set timeout -1
-        spawn bash -c "gpg --pinentry-mode loopback --passphrase='' --generate-key"
-        expect "Real name: "
-        send -- "$NAME\n"
-        expect "Email address: "
-        send -- "$EMAIL\n"
-        expect "Change (N)ame, (E)mail, or (O)kay/(Q)uit? "
-        send -- "o\r"
-        expect EOF
-END
+
+    EXPECT_FILE="expectsecrets.expect"
+    GPG_FILE="gpg_create.sh"
+  touch "${EXPECT_FILE}"
+  cat > "${EXPECT_FILE}" <<EOF
+#!/usr/bin/expect
+set timeout -1
+spawn ./${GPG_FILE}
+expect "Real name: "
+send -- "${NAME}\n"
+expect "Email address: "
+send -- "${EMAIL}\n"
+expect "Change (N)ame, (E)mail, or (O)kay/(Q)uit? "
+send -- "o\r"
+expect EOF
+EOF
+
+  touch "${GPG_FILE}"
+    cat > "${GPG_FILE}" <<EOF
+#!/bin/bash
+gpg --pinentry-mode loopback --passphrase='' --generate-key
+EOF
+    chmod +x "${EXPECT_FILE}"
+    chmod +x "${GPG_FILE}"
+
+    ./"${EXPECT_FILE}"
+    #
 }
 
 function generate_keys() {
   KEY_LIST=$(gpg --list-secret-keys)
 
   if [[ "${KEY_LIST}" != *"${EMAIL}"* ]]; then
-    $(createKey) &
+    $(createKey)
   fi
 
   #Test the keystore for the signing key creation
@@ -46,11 +62,12 @@ function generate_keys() {
       i=$max
     fi
   done
-  
   #Terraform requires a JSON response from a script
   JSON_STRING_RESULT=$( jq -n --arg signing_key "$SIGNING_KEY" --arg public_key "$PUBLIC_CERTIFICATE" '{signingkey: $signing_key, publickey: $public_key}' )
 
   #return response
+  rm -rf "${EXPECT_FILE}"
+  rm -rf "${GPG_FILE}"
   echo "${JSON_STRING_RESULT}"
 }
 
